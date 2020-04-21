0c13acd1574288474d8338ce8ac2d44576bd8ea4
rickovirnanda <55388386+rickovirnanda@users.noreply.github.com>
2020-04-21 22:58:52 +0700

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-tutorial';
+  
+  nama:string = "";
+  telepon:number = 0;
+  isAvailable:boolean = false;
+  NIK:number = 0;
+  tanggal:Date = null;
+  skill:string[] = [];
+  nilai:number[] = [];
+
+  constructor()
+  {
+    this.nama = "Miko Kucing olen";
+    this.telepon = 123454;
+    this.isAvailable = true;
+    this.NIK = 390439394849;
+    this.tanggal = new Date();
+    this.skill = ['Koding', 'Makan', 'tidur'];
+    this.nilai = [4,2,4,5,3];
+
+    console.log(this.nama);
+    console.log(this.telepon);
+    console.log(this.isAvailable);
+    console.log(this.NIK);
+    console.log(this.tanggal.getDate());
+    
+    this.skill.forEach(
+      (item)=>{
+        console.log(item);
+      }
+    );
+    this.nilai.forEach(
+      (data)=>console.log(data)
+    )
+  }
 }
